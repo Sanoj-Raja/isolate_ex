fa28a9bc93bcabcc8d580b18153044db4be8f72a
Sanoj Raja <sanojp@triazinesoft.com>
2024-07-03 11:20:27 +0530

    Created isolates for heavy tasks.

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,23 +1,55 @@
+import 'dart:isolate';
 import 'package:get/get.dart';
+import 'package:flutter/material.dart';
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
-
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+  void computeHeavyTask() {
+    _heavyTask(HeavyTaskExecutionType.withoutAnyIsolates);
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  Future<void> computeHeavyTaskWithIsolatesRunMethod() async {
+    await Isolate.run(
+      () {
+        _heavyTask(HeavyTaskExecutionType.withIsolatesRunMethod);
+      },
+    );
   }
 
-  @override
-  void onClose() {
-    super.onClose();
+  Future<void> computeHeavyTaskWithIsolatesSpawnMethod() async {
+    final port = ReceivePort();
+    await Isolate.spawn<SendPort>(
+      (sendPort) {
+        _heavyTask(
+          HeavyTaskExecutionType.withIsolatesSpawnMethod,
+          sendPort: sendPort,
+        );
+      },
+      port.sendPort,
+    );
+
+    port.listen(
+      (message) {
+        debugPrint('How heavy task executed ---> $message');
+      },
+    );
   }
 
-  void increment() => count.value++;
+  void _heavyTask(HeavyTaskExecutionType executionType, {SendPort? sendPort}) {
+    for (int i = 0; i < 10000000000; i++) {}
+    executionType == HeavyTaskExecutionType.withIsolatesRunMethod
+        ? debugPrint('Heavy task completed with isolates run method.')
+        : executionType == HeavyTaskExecutionType.withIsolatesSpawnMethod
+            ? debugPrint('Heavy task completed with isolates spawn method.')
+            : debugPrint('Heavy task completed without any isolates.');
+
+    if (sendPort != null) {
+      sendPort.send('Heavy task completed with isolates spawn method.');
+    }
+  }
+}
+
+enum HeavyTaskExecutionType {
+  withoutAnyIsolates,
+  withIsolatesRunMethod,
+  withIsolatesSpawnMethod,
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,22 +1,62 @@
-import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
+import 'package:lottie/lottie.dart';
+import 'package:flutter/material.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
-  const HomeView({Key? key}) : super(key: key);
+  const HomeView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HomeView'),
+        title: const Text(
+          'Isolates Example App',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Lottie.asset('assets/loading.json'),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: controller.computeHeavyTask,
+              child: const Text(
+                'Run Heavy Task',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: controller.computeHeavyTaskWithIsolatesRunMethod,
+              child: const Text(
+                'Run Heavy Task with Isolates (Run)',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: controller.computeHeavyTaskWithIsolatesSpawnMethod,
+              child: const Text(
+                'Run Heavy Task with Isolates (Spawn)',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
